74ace24af24e66fe0bd3c517c418dd047f1e7447
radfre <schatz.alex@gmail.com>
2023-05-03 22:06:55 -0500

    bad GUI

--- script/GUI.py
+++ script/GUI.py
@@ -6,15 +6,28 @@ class GUI:
         # Create window
         self.window = tk.Tk()
         self.window.title('Reddit Top Posts')
-        self.window.geometry('400x800')
+        self.window.geometry('800x500')
+        self.window.configure(bg='gray')
 
         # Create label to display results
-        self.result_label = tk.Label(self.window, text='', justify='left')
-        self.result_label.pack()
+        self.Title_label = tk.Label(self.window, text='', justify='left')
+        self.Top_label = tk.Label(self.window, text='', justify='left')
+        self.Users_label = tk.Label(self.window, text='', justify='left')
+
+        self.frame1 = tk.Frame(self.window, tk.GROOVE, borderwidth=10)
+
+        self.Title_label.pack(self.frame1)
+        self.Top_label.pack(self.frame1)
+        self.Users_label(self.frame1)
+
+        self.frame1.pack(tk.LEFT)
+
+        self.Users_label.pack()
+
 
     def loading(self):
         print('loading')
 
     def display_top_posts(self, top_posts_text):
-        self.result_label.config(text=top_posts_text, justify='left')
+        self.Top_label.config(text=top_posts_text)
         self.window.mainloop()
